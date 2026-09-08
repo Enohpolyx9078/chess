00a890f136de085551e3490d8a53d49cac74c6b4
Enohpolyx9078 <hicrawford982@gmail.com>
2026-09-08 12:25:16 -0600

    Created MoveStrategy abstract class

---
+++ shared/src/main/java/chess/movement/MoveStrategy.java
@@ -0,0 +1,15 @@
+package chess.movement;
+
+import chess.*;
+
+import java.util.Collection;
+
+public abstract class MoveStrategy {
+    protected MovementValidator v;
+
+    public MoveStrategy(MovementValidator v) {
+        this.v = v;
+    }
+
+    abstract Collection<ChessMove> getValidMoves(ChessPosition p, ChessBoard b);
+}
