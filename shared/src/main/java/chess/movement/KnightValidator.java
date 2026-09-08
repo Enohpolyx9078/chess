f172fc8096f3dde30e6cfaa2cc5d1cf8c39b8741
Enohpolyx9078 <hicrawford982@gmail.com>
2026-09-08 12:45:11 -0600

    Implement validity check in KnightValidator

--- shared/src/main/java/chess/movement/KnightValidator.java
+++ shared/src/main/java/chess/movement/KnightValidator.java
@@ -4,6 +4,7 @@ import chess.ChessGame;
 import chess.ChessMove;
 import chess.ChessPosition;
 import chess.ChessBoard;
+import chess.ChessPiece;
 
 public class KnightValidator implements MovementValidator {
 
@@ -15,14 +16,20 @@ public class KnightValidator implements MovementValidator {
     public boolean isValid(ChessBoard b, ChessMove move, ChessPosition p, ChessGame.TeamColor color) {
         boolean valid = true;
 
-        //TODO Check if the requested move is valid given p and color
+        // Check if the requested move is valid given p and color
         final int[] posData = ChessBoard.interpretChessPosition(p);
         final int targetRow = posData[0];
         final int targetCol = posData[1];
 
-        // A move is valid if:
-        //  The endPosition is on the board
-        //  The endPosition is not on top of a piece of color
+        // Check out of bounds
+        if ((targetRow < 0 || targetCol < 0) || (targetRow >= b.getSize() || targetCol >= b.getSize())) {
+            return false;
+        }
+        // Check capturing
+        ChessPiece targetPiece = b.getPiece(p);
+        if (targetPiece == null || targetPiece.getTeamColor() == color) {
+            return false;
+        }
 
         return valid;
     }
