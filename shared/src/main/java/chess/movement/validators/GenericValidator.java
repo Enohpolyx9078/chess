a0297dad078048ca0c27f91fc0b4ff305c79175d
Enohpolyx9078 <hicrawford982@gmail.com>
2026-09-09 10:20:24 -0600

    Abstracted generic validator's stuff into the movement validator class

--- shared/src/main/java/chess/movement/validators/GenericValidator.java
+++ shared/src/main/java/chess/movement/validators/GenericValidator.java
@@ -11,21 +11,6 @@ public class GenericValidator implements MovementValidator {
 
     @Override
     public boolean isValid(ChessBoard b, ChessMove move, ChessGame.TeamColor color) {
-        // Check if the requested move is valid given move and color
-        final int[] posData = ChessBoard.interpretChessPosition(move.getEndPosition());
-        final int targetRow = posData[0];
-        final int targetCol = posData[1];
-
-        // Check out of bounds
-        if ((targetRow < 0 || targetCol < 0) || (targetRow >= b.getSize() || targetCol >= b.getSize())) {
-            return false;
-        }
-        // Check capturing
-        ChessPiece targetPiece = b.getPiece(move.getEndPosition());
-        boolean targetOccupied = targetPiece != null;
-        if (targetOccupied) {
-            return targetPiece.getTeamColor() != color;
-        }
-        return true;
+        return MovementValidator.checkBasic(b, move, color);
     }
 }
--- shared/src/main/java/chess/movement/validators/MovementValidator.java
+++ shared/src/main/java/chess/movement/validators/MovementValidator.java
@@ -3,7 +3,27 @@ package chess.movement.validators;
 import chess.ChessGame;
 import chess.ChessMove;
 import chess.ChessBoard;
+import chess.ChessPiece;
 
 public interface MovementValidator {
+    // Performs the basic checks of out-of-bounds and overlapping your own team
+    static boolean checkBasic(ChessBoard b, ChessMove move, ChessGame.TeamColor color) {
+        final int[] posData = ChessBoard.interpretChessPosition(move.getEndPosition());
+        final int targetRow = posData[0];
+        final int targetCol = posData[1];
+
+        // Check out of bounds
+        if ((targetRow < 0 || targetCol < 0) || (targetRow >= b.getSize() || targetCol >= b.getSize())) {
+            return false;
+        }
+        // Check capturing
+        ChessPiece targetPiece = b.getPiece(move.getEndPosition());
+        boolean targetOccupied = targetPiece != null;
+        if (targetOccupied) {
+            return targetPiece.getTeamColor() != color;
+        }
+        return true;
+    }
+
     boolean isValid(ChessBoard b, ChessMove move, ChessGame.TeamColor color);
 }
