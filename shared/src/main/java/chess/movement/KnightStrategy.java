a72855aedb9cc286b1ee6ff873f0da92791dba12
Enohpolyx9078 <hicrawford982@gmail.com>
2026-09-08 20:56:06 -0600

    Further generalized MoveStrategy for better use in other subclasses

--- shared/src/main/java/chess/movement/KnightStrategy.java
+++ shared/src/main/java/chess/movement/KnightStrategy.java
@@ -10,35 +10,12 @@ import java.util.Collection;
 import java.util.List;
 
 public class KnightStrategy extends MoveStrategy {
-    private final int[][] offsets = {
+    private static final int[][] offsets = {
             {2, 1}, {2, -1}, {1, 2}, {1, -2},
             {-2, 1}, {-2, -1}, {-1, 2}, {-1, -2}
     };
 
     public KnightStrategy(MovementValidator v) {
-        super(v);
-    }
-
-    private ChessMove deriveTarget(ChessPosition p, int[] offset) {
-        final int[] posData = ChessBoard.interpretChessPosition(p);
-        final int curRow = posData[0];
-        final int curCol = posData[1];
-        final ChessPosition end = new ChessPosition(curRow + offset[0] + 1, curCol + offset[1] + 1);
-        return new ChessMove(p, end, null);
-    }
-
-    @Override
-    public Collection<ChessMove> getValidMoves(ChessPosition p, ChessBoard b, ChessGame.TeamColor color) {
-        // get a list of ChessMoves for any move that is valid
-        // for each offset, derive the target move
-        // if the target move is valid, add it to the final collection
-        List<ChessMove> validMoves = new ArrayList<>();
-        for (int[] offset : offsets) {
-            ChessMove target = deriveTarget(p, offset);
-            if (v.isValid(b, target, color)) {
-                validMoves.add(target);
-            }
-        }
-        return validMoves;
+        super(v, offsets);
     }
 }
--- shared/src/main/java/chess/movement/MoveStrategy.java
+++ shared/src/main/java/chess/movement/MoveStrategy.java
@@ -5,16 +5,40 @@ import chess.ChessMove;
 import chess.ChessBoard;
 import chess.ChessGame;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 public abstract class MoveStrategy {
     // Child classes should have a Collection<List<Integer>> offsets parameter
     // The child objects in the List should look like {int rowMove, int colMove}
     protected MovementValidator v;
+    protected int[][] offsets;
 
-    public MoveStrategy(MovementValidator v) {
+    public MoveStrategy(MovementValidator v, int[][] offsets) {
         this.v = v;
+        this.offsets = offsets;
     }
 
-    public abstract Collection<ChessMove> getValidMoves(ChessPosition p, ChessBoard b, ChessGame.TeamColor color);
+    private ChessMove deriveTarget(ChessPosition p, int[] offset) {
+        final int[] posData = ChessBoard.interpretChessPosition(p);
+        final int curRow = posData[0];
+        final int curCol = posData[1];
+        final ChessPosition end = new ChessPosition(curRow + offset[0] + 1, curCol + offset[1] + 1);
+        return new ChessMove(p, end, null);
+    }
+
+    public Collection<ChessMove> getValidMoves(ChessPosition p, ChessBoard b, ChessGame.TeamColor color) {
+        // get a list of ChessMoves for any move that is valid
+        // for each offset, derive the target move
+        // if the target move is valid, add it to the final collection
+        List<ChessMove> validMoves = new ArrayList<>();
+        for (int[] offset : offsets) {
+            ChessMove target = deriveTarget(p, offset);
+            if (v.isValid(b, target, color)) {
+                validMoves.add(target);
+            }
+        }
+        return validMoves;
+    }
 }
