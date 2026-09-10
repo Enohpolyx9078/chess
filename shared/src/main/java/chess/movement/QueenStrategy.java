d35c9e660bfdd2194c621d9bc6f3e51e198dfbd4
Enohpolyx9078 <hicrawford982@gmail.com>
2026-09-09 22:04:36 -0600

    Created Queen Strategy

---
+++ shared/src/main/java/chess/movement/QueenStrategy.java
@@ -0,0 +1,19 @@
+package chess.movement;
+
+import chess.movement.validators.MovementValidator;
+
+public class QueenStrategy extends MoveStrategy{
+    private static final int[][] offsets = {
+            {1, 0}, {2, 0}, {3, 0}, {4, 0}, {5, 0}, {6, 0}, {7, 0},
+            {-1, 0}, {-2, 0}, {-3, 0}, {-4, 0}, {-5, 0}, {-6, 0}, {-7, 0},
+            {0, 1}, {0, 2}, {0, 3}, {0, 4}, {0, 5}, {0, 6}, {0, 7},
+            {0, -1}, {0, -2}, {0, -3}, {0, -4}, {0, -5}, {0, -6}, {0, -7},
+            {1, 1}, {2, 2}, {3, 3}, {4, 4}, {5, 5}, {6, 6}, {7, 7},
+            {-1, 1}, {-2, 2}, {-3, 3}, {-4, 4}, {-5, 5}, {-6, 6}, {-7, 7},
+            {1, -1}, {2, -2}, {3, -3}, {4, -4}, {5, -5}, {6, -6}, {7, -7},
+            {-1, -1}, {-2, -2}, {-3, -3}, {-4, -4}, {-5, -5}, {-6, -6}, {-7, -7}
+    };
+    public QueenStrategy(MovementValidator v) {
+        super(v, offsets);
+    }
+}
