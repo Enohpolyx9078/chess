e0cbb23d272be9c6de259bb545cc9a955f3eb0ba
Enohpolyx9078 <hicrawford982@gmail.com>
2026-09-14 14:54:50 -0600

    Stubbed out EndGameEnforcer logic

--- shared/src/main/java/chess/rules/EndGameEnforcer.java
+++ shared/src/main/java/chess/rules/EndGameEnforcer.java
@@ -2,20 +2,56 @@ package chess.rules;
 
 import chess.ChessBoard;
 import chess.ChessGame;
+import chess.ChessPosition;
 
 public class EndGameEnforcer implements Enforcer {
+    private static ChessPosition findKing(ChessGame.TeamColor color, ChessBoard board) {
+        //TODO find the king's position and return it
+        // for each space on the board
+        //    if the space is not null
+        //        if the piece is color && piece is the the king
+        //            return the current position
+        // throw missing king error
+        throw new RuntimeException("Not Implemented");
+    }
+
+    private static boolean kingIsTrapped(ChessGame.TeamColor color, ChessBoard board) {
+        //TODO check if every space the king can to move would put it in check
+        // find the position of color's king
+        // for each space that's a move for the king
+        //    create a hypothetical board where this move happened
+        //    if !isInCheck(color, hypothetical)
+        //        return false
+        // return true
+        throw new RuntimeException("Not Implemented");
+    }
+
     @Override
      public boolean isInCheck(ChessGame.TeamColor color, ChessBoard board) {
+        //TODO check if color is in check
+        // find the position of color's king
+        // for each piece on the opposing team
+        //    if a valid move lands on the king
+        //        return true
+        // return false
         throw new RuntimeException("Not Implemented");
     }
 
     @Override
     public boolean isInCheckmate(ChessGame.TeamColor color, ChessBoard board) {
+        //TODO find the position of color's king
+        // if every place the king could move to is in check && the king is in check
+        //    return true
+        // return false
         throw new RuntimeException("Not Implemented");
     }
 
     @Override
     public boolean isInStalemate(ChessGame.TeamColor color, ChessBoard board) {
+        //TODO find the position of color's king
+        // if every place the king could move to is in check && the king is NOT in check
+        //    return true
+        // return false
         throw new RuntimeException("Not Implemented");
     }
 }
